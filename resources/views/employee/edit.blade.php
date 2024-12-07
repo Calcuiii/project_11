3c59b7c8b534c7358ce15414b1ddb4ff7c2fe924
khancit02 <soniaamelia4704@gmail.com>
2024-12-07 08:32:46 +0700

    first commit

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -1,161 +1,84 @@
-<?php
+@extends('layouts.app')
 
-namespace App\Http\Controllers;
+@section('content')
+    <div class="container-sm my-5">
+        <div class="row justify-content-center">
+            <div class="p-5 bg-light rounded-3 col-xl-6 border">
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\DB;
+<form action="{{ route('employees.update', ['employee' => $employee->id]) }}" method="POST">
+    @csrf
+    @method('put')
+    <div class="row justify-content-center">
+        <div class="p-5 bg-light rounded-3 col-xl-6">
+            <div class="mb-3 text-center">
+                <i class="bi-person-circle fs-1"></i>
+                <h4>Edit Employee</h4>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label for="firstName" class="form-label">First Name</label>
+                    <input class="form-control @error('firstName') is-invalid @enderror" type="text" name="firstName" id="firstName" value="{{ $errors->any() ? old('firstName') : $employee->firstname }}" placeholder="Enter First Name">
+                    @error('firstName')
+                        <div class="text-danger"><small>{{ $message }}</small></div>
+                    @enderror
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="lastName" class="form-label">Last Name</label>
+                    <input class="form-control @error('lastName') is-invalid @enderror" type="text" name="lastName" id="lastName" value="{{ $errors->any() ? old('lastName') : $employee->lastname }}" placeholder="Enter Last Name">
+                    @error('lastName')
+                        <div class="text-danger"><small>{{ $message }}</small></div>
+                    @enderror
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" id="email" value="{{ $errors->any() ? old('email') : $employee->email }}" placeholder="Enter Email">
+                    @error('email')
+                        <div class="text-danger"><small>{{ $message }}</small></div>
+                    @enderror
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="age" class="form-label">Age</label>
+                    <input class="form-control @error('age') is-invalid @enderror" type="text" name="age" id="age" value="{{ $errors->any() ? old('age') : $employee->age }}" placeholder="Enter Age">
+                    @error('age')
+                        <div class="text-danger"><small>{{ $message }}</small></div>
+                    @enderror
+                </div>
+                <div class="col-md-12 mb-3">
+                    <label for="position" class="form-label">Position</label>
+                    <select name="position" id="position" class="form-select">
+                        @php
+                            $selected = "";
+                            if ($errors->any())
+                                $selected = old('position');
+                            else
+                                $selected = $employee->position_id;
+                        @endphp
+                        @foreach ($positions as $position)
+                            <option value="{{ $position->id }}" {{ $selected == $position->id ? 'selected' : '' }}>{{ $position->code.' - '.$position->name }}</option>
+                        @endforeach
+                    </select>
+                    @error('position')
+                        <div class="text-danger"><small>{{ $message }}</small></div>
+                    @enderror
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-6 d-grid">
+                    <a href="{{ route('employees.index') }}" class="btn btn-outline-dark btn-lg mt-3"><i class="bi-arrow-left-circle me-2"></i> Cancel</a>
+                </div>
+                <div class="col-md-6 d-grid">
+                    <button type="submit" class="btn btn-dark btn-lg mt-3"><i class="bi-check-circle me-2"></i> Edit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
 
-class EmployeeController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $pageTitle = 'Employee List';
 
-        // QUERY BUILDER
-        $employees = DB::table('employees')
-            ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
-            ->select('employees.*', 'employees.id as employee_id', 'positions.name as position_name')
-            ->get();
+            </div>
+        </div>
+    </div>
 
-        return view('employee.index', [
-            'pageTitle' => $pageTitle,
-            'employees' => $employees
-        ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        $pageTitle = 'Create Employee';
-
-        // QUERY BUILDER
-        $positions = DB::table('positions')->get();
-
-        return view('employee.create', compact('pageTitle', 'positions'));
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $messages = [
-            'required' => ':Attribute harus diisi.',
-            'email' => 'Isi :attribute dengan format yang benar',
-            'numeric' => 'Isi :attribute dengan angka'
-        ];
-
-        $validator = Validator::make($request->all(), [
-            'firstName' => 'required',
-            'lastName' => 'required',
-            'email' => 'required|email',
-            'age' => 'required|numeric',
-        ], $messages);
-
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator)->withInput();
-        }
-
-        // QUERY BUILDER
-        DB::table('employees')->insert([
-            'firstname' => $request->firstName,
-            'lastname' => $request->lastName,
-            'email' => $request->email,
-            'age' => $request->age,
-            'position_id' => $request->position,
-        ]);
-
-        return redirect()->route('employees.index');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        $pageTitle = 'Employee Detail';
-
-        // QUERY BUILDER
-        $employee = DB::table('employees')
-            ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
-            ->select('employees.*', 'employees.id as employee_id', 'positions.name as position_name')
-            ->where('employees.id', $id)
-            ->first();
-
-        return view('employee.show', compact('pageTitle', 'employee'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        $pageTitle = 'Edit Employee';
-
-        // QUERY BUILDER
-        $employee = DB::table('employees')
-            ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
-            ->select('employees.*', 'positions.name as position_name')
-            ->where('employees.id', $id)
-            ->first();
-
-        $positions = DB::table('positions')->get();
-
-        return view('employee.edit', compact('pageTitle', 'employee', 'positions'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        $messages = [
-            'required' => ':Attribute harus diisi.',
-            'email' => 'Isi :attribute dengan format yang benar.',
-            'numeric' => 'Isi :attribute dengan angka.'
-        ];
-
-        $validator = Validator::make($request->all(), [
-            'firstName' => 'required',
-            'lastName' => 'required',
-            'email' => 'required|email',
-            'age' => 'required|numeric',
-        ], $messages);
-
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator)->withInput();
-        }
-
-        // QUERY BUILDER
-        DB::table('employees')
-            ->where('id', $id)
-            ->update([
-                'firstname' => $request->firstName,
-                'lastname' => $request->lastName,
-                'email' => $request->email,
-                'age' => $request->age,
-                'position_id' => $request->position,
-            ]);
-
-        return redirect()->route('employees.index');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        // QUERY BUILDER
-        DB::table('employees')
-            ->where('id', $id)
-            ->delete();
-
-        return redirect()->route('employees.index');
-    }
-}
+    @endsection
